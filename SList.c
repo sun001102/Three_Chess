160839a40f4d4797db5dae0c2342f0b9d0f3faef
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-09-05 21:50:33 +0800

    DS(4)

--- SList.c
+++ SList.c
@@ -28,3 +28,62 @@ void SListPushFront(SLTNode** pphead, SLTDataType x)//头插
 		*pphead = newnode;
 	}
 }
+void SListPushBack(SLTNode** pphead, SLTDataType x)
+{
+	assert(pphead);
+	//1.空
+	//2.非空
+	SLTNode* newnode = BuySLTNode(x);
+	if (*pphead == NULL)
+	{
+		*pphead = newnode;
+	}
+	else
+	{//找尾
+		SLTNode* tail = *pphead;//指向第一个节点
+		while (tail->next != NULL)//找尾
+		{
+			tail = tail->next;
+		}
+		tail->next = newnode;
+	}
+}
+void SListPopFront(SLTNode** pphead)
+{
+	assert(pphead);
+	//删光了？//温柔的检查
+	if (*pphead == NULL)
+	{
+		return;
+	}
+	assert(*pphead != NULL);//暴力的检查
+	SLTNode* del = *pphead;
+	*pphead =(* pphead)->next;
+	free(del);
+	del = NULL;
+}
+void SListPopBack(SLTNode** pphead)
+{
+	assert(pphead);
+	assert(*pphead != NULL);
+	//一个节点
+	// 多个节点
+	if ((*pphead)->next == NULL)
+	{
+		*pphead = NULL;
+	}
+	else
+	{
+		//找尾
+		SLTNode* perv = NULL;
+		SLTNode* tail = *pphead;
+		while (tail->next != NULL)
+		{
+			perv = tail;
+			tail = tail->next;
+		}
+		perv->next = NULL;
+		free(tail);
+		tail = NULL;
+	}
+}
