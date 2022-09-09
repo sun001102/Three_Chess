e6ecd6dc8646432ace2e8c0a74949f618d823951
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-09-09 21:48:34 +0800

    链表测试

--- test.c
+++ test.c
@@ -1,33 +1,67 @@
-#include "SeqList.h"
-void TestSeqList1()
+#include <stdio.h>
+struct ListNode
 {
-	SL s;
-	SLInit(&s);
-	SLPushBack(&s, 1);
-	SLPushBack(&s, 2);
-	SLPushBack(&s, 4);
-	SLPushBack(&s, 6);
-	SLPushBack(&s, 8);
-
-	SLPrint(&s);
-    
-
-	SLErase(&s, 2);
-	SLPrint(&s);
-	SLInsert(&s, 3, 20);
-	SLPrint(&s);
-
-	SLDestory(&s);
+	int val;
+	struct ListNode* next;
+};
+struct ListNode* removeElements(struct ListNode* head, int val) {
+	struct ListNode* cur = head;
+	struct ListNode* newhead = NULL, * tail = NULL;
+	while (cur)
+	{
+		if (cur->val != val)
+		{
+			if (tail == NULL)
+			{
+				newhead = tail = cur;
+			}
+			else
+			{
+				tail->next = cur;
+				tail=tail->next;
+			}
+			cur = cur->next;
+		}
+		else
+		{
+			struct ListNode* del = cur;
+			cur = cur->next;
+			free(del);
+		}
+	}
+	return newhead;
 }
-//void TestSeqList()
-//{
-//
-//}
-
-
-
 int main()
 {
-	TestSeqList1();
+	struct ListNode* n1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n1);
+	struct ListNode* n2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n2);
+	struct ListNode* n3 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n3);
+	struct ListNode* n4= (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n4);
+	struct ListNode* n5 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n5);
+	struct ListNode* n6 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n6);
+	struct ListNode* n7 = (struct ListNode*)malloc(sizeof(struct ListNode));
+	assert(n7);
+	n1->next = n2;
+    n2->next = n3;
+	n3->next = n4;
+	n4->next = n5;
+	n5->next = n6;
+	n6->next = n7;
+	n7->next =NULL;
+
+	n1->val = 1;
+	n2->val = 2;
+	n3->val = 6;
+	n4->val = 3;
+	n5->val = 4;
+	n6->val = 5;
+	n7->val = 6;
+	struct ListNode* head = removeElements(n1, 6);
 	return 0;
 }
